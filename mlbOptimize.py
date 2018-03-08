e094d3a3ea34f63e308d53c693c8a5d972092d38
Zachary Duncan <zduncan27@gmail.com>
2018-03-08 15:22:34 -0500

    add csv writing capability

--- mlbOptimize.py
+++ mlbOptimize.py
@@ -254,6 +254,27 @@ def lineups(numLineups):
         lineupList.append(results[0])
         resultList.append(results)
 
+    lineupsOnly = [['P', 'P', 'C', '1B', '2B', '3B', 'SS', 'OF', 'OF', 'OF']]
+
+    for i in range(1, numLineups):
+        lineupsOnly.append(resultList[i][0])
+
+    # Create csv file of lineups
+    myFile = open('lineups.csv', 'w')
+    with myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(lineupsOnly)
+
+    print("Writing complete")
+
+    # Create csv file of lineups with additional results
+    mF2 = open('lineupsWithResults.csv', 'w')
+    with mF2:
+        writer = csv.writer(mF2)
+        writer.writerows(resultList)
+
+    print("Writing complete")
+
     return resultList
 
 print(lineups(10))
